65475d7d96c6f9711f32aee93c9d0e5347f0572f
Andreas Neukoetter <andreas@omni-mad.com>
2013-01-02 13:26:24 +0100

    + Added noise texture.

--- PhoneDirect3DApp2/CubeRenderer.cpp
+++ PhoneDirect3DApp2/CubeRenderer.cpp
@@ -110,16 +110,55 @@ void CubeRenderer::CreateDeviceResources()
 
 	float pData[ 128*128*4 ];
 
+	const int rw = 8*8;
+	const int rh = 8*8;
+	float pRandom[ rw*rh ];
+	for( int y=0; y<rh; ++y )
+	{
+		for( int x=0; x<rw; ++x )
+		{
+			pRandom[ y*rw + x ] = ( float )rand()/RAND_MAX;
+		}
+	}
+
 	for( int y=0; y<128; ++y )
 	{
 		for( int x=0; x<128; ++x )
 		{
+			float rx = ( x*( rw-1 ) )/128.0f;
+			float ry = ( y*( rh-1 ) )/128.0f;
+
+			int rix = ( int )rx;
+			float rfx = rx-rix;
+
+			int riy = (int)ry;
+			float rfy = ry-riy;
+
+			float corners[ 4 ];
+			corners[ 0 ] = pRandom[ ( int )riy*rw + rix ];
+			corners[ 1 ] = pRandom[ ( int )riy*rw + rix+1 ];
+			corners[ 2 ] = pRandom[ ( int )( riy+1 )*rw + rix ];
+			corners[ 3 ] = pRandom[ ( int )( riy+1 )*rw + rix+1 ];
+
+			float rl = pRandom[ ( int )ry*rw + rix ];
+			float rr = pRandom[ ( int )ry*rw + rix+1 ];
+//			float r = ( 1-rfx )*rl+( rfx )*rr;
+
+			float upper = ( 1-rfx )*corners[ 0 ]+( rfx )*corners[ 1 ];
+			float lower = ( 1-rfx )*corners[ 2 ]+( rfx )*corners[ 3 ];
+			float r = ( 1-rfy )*upper+( rfy )*lower;
+
+			float u = ( x-64 )/128.0f;
+			float v = ( y-64 )/128.0f;
 			int i = 128*y+x;
-//			pData[ i*4+0 ] = 0*( x%128 )/128.0f;
-//			pData[ i*4+1 ] = 0*( y%128 )/128.0f;
-			pData[ i*4+0 ] = 1*( (x+y)%256 )/256.0f;
-			pData[ i*4+1 ] = 1*( (x+y)%256 )/256.0f;
-			pData[ i*4+2 ] = 1*( (x+y)%256 )/256.0f;
+//			pData[ i*4+0 ] = 1*( sinf( 2*3.14f*u )+sinf( 2*3.14f*v ) );
+			float c = ( 0.25f*( u*u+v*v )+0.75f )*( 0.5f*r+0.5f );
+			pData[ i*4+0 ] = c;
+			pData[ i*4+1 ] = c;
+			pData[ i*4+2 ] = c;
+//			pData[ i*4+0 ] = 1*( (x+y)%256 )/256.0f;
+//			pData[ i*4+1 ] = 1*( (x+y)%256 )/256.0f;
+//			pData[ i*4+2 ] = 1*( (x+y)%256 )/256.0f;
 			pData[ i*4+3 ] = 1.0;
 		}
 	}
